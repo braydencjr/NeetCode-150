125c043b171fb7e9b2dc5747a5129de7f918da77
Brayden <braydencjr05@gmail.com>
2026-06-16 12:29:29 +0800

    Time: 72 ms (19.57%), Space: 48.4 MB (7.82%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        HashMap <Character,Integer> freq1 = new HashMap<>();
-        HashMap <Character,Integer> freq2 = new HashMap<>();
-        for(int i=0 ; i<word1.length() ; i++){
-            freq1.put(word1.charAt(i), freq1.getOrDefault(word1.charAt(i), 0) + 1);
+
+        HashMap<Character, Integer> freq1 = new HashMap<>();
+        HashMap<Character, Integer> freq2 = new HashMap<>();
+
+        for (char c : word1.toCharArray()) {
+            freq1.put(c, freq1.getOrDefault(c, 0) + 1);
         }
 
-        for(int i=0 ; i<word2.length() ; i++){
-            freq2.put(word2.charAt(i), freq2.getOrDefault(word2.charAt(i), 0) + 1);
+        for (char c : word2.toCharArray()) {
+            freq2.put(c, freq2.getOrDefault(c, 0) + 1);
         }
 
-        if(!freq1.keySet().equals(freq2.keySet())){
+        if (!freq1.keySet().equals(freq2.keySet())) {
             return false;
         }
 
-        ArrayList<Integer> list1 = new ArrayList<>();
-        ArrayList<Integer> list2 = new ArrayList<>();
-
-        for(int value : freq1.values()){
-            list1.add(value);
-        }
-
-        for(int value : freq2.values()){
-            list2.add(value);
-        }
+        ArrayList<Integer> list1 = new ArrayList<>(freq1.values());
+        ArrayList<Integer> list2 = new ArrayList<>(freq2.values());
 
         Collections.sort(list1);
         Collections.sort(list2);
